10c060af98d6647ceba995be63d13cddfe3a612e
epiglotis <83986620+epiglotis@users.noreply.github.com>
2023-03-14 19:32:51 +0300

    Hotel & User Models and Routes Created

---
+++ api/controllers/hotelController.js
@@ -0,0 +1,48 @@
+import HotelModel from "../models/HotelModel.js";
+
+export const createHotel = async (req, res, next) => {
+  const newHotel = new HotelModel(req.body);
+
+  try {
+    const savedHotel = await newHotel.save();
+    res.status(200).json(savedHotel);
+  } catch (error) {
+    next(error);
+  }
+};
+export const updateHotel = async (req, res, next) => {
+  try {
+    const updatedHotel = await HotelModel.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(updatedHotel);
+  } catch (error) {
+    next(error);
+  }
+};
+export const deleteHotel = async (req, res, next) => {
+  try {
+    await HotelModel.findByIdAndDelete(req.params.id);
+    res.status(200).json('Hotel has been deleted.');
+  } catch (error) {
+    next(error);
+  }
+};
+export const getHotel = async (req, res, next) => {
+  try {
+    const hotel = await HotelModel.findById(req.params.id);
+    res.status(200).json(hotel);
+  } catch (error) {
+    next(error);
+  }
+};
+export const getHotels = async (req, res, next) => {
+  try {
+    const hotels = await HotelModel.find();
+    res.status(200).json(hotels);
+  } catch (error) {
+    next(error);
+  }
+};
