3255ccf6f66afad6be18d3310f367d5b567f62aa
epiglotis <83986620+epiglotis@users.noreply.github.com>
2023-04-04 00:33:54 +0300

    Basic Layout and Navbar Integrated

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Hotel from './Pages/Hotel/Hotel';
+import List from './Pages/List/List';
+
 function App() {
   return (
-    <div>
-      Hello world!
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/hotels' element={<List/>}/>
+        <Route path='/hotels/:id' element={<Hotel/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
